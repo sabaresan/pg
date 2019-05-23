77cc62ecba4b19ef6f363ae4981754fde6521054
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-23 20:49:19 +0530

    Add files via upload

---
+++ ss.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+#include<string.h>
+
+int main()
+{
+   
+char s[1000];
+   
+int n,i;
+   
+scanf("%s%d",&s,&n);
+   
+for(i=0;i<n;i++)
+   
+{
+       
+printf("%s ",s);
+   
+printf("\n");
+   
+}
+   
+
+}
