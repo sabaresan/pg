a5ef0b28735eea8b36858726acca3bc14a401068
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-23 14:53:26 +0530

    Add files via upload

---
+++ line.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class line {
+    
+public static int getlinecount(String text){
+        
+return text.split("[\n|\r]").length;
+    
+}
+	
+public static void main (String a[]) 
+{
+	    
+String str;
+	    
+Scanner sc=new Scanner(System.in);
+	    
+str=sc.nextLine();
+        
+int count=getlinecount(str);
+        S
+System.out.println(" "+count);
+	
+}
+
+}
