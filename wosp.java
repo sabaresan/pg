ac31d77cae4ec16edd047086ec3487b84f1a81f3
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-23 14:04:51 +0530

    Add files via upload

---
+++ wosp.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+
+public class wosp
+{
+	
+
+public static void main (String[] args) 
+	
+
+{
+
+String s;
+int count=0;
+
+Scanner sc=new Scanner(System.in);
+
+s=sc.nextLine();
+
+for(int i=0;i<s.length();i++)
+
+{
+    
+if(s.charAt(i)!=' ')
+    
+count++;
+
+}
+
+System.out.println(" "+count);
+
+
+}
+
+}
+	  
