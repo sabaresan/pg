02213ce69aeec7ff58d9117653cc3bfc872bb2f3
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-23 16:00:11 +0530

    Add files via upload

---
+++ strc.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    
+char s1[50],s2[50];
+    
+int i;
+    
+scanf("%s%s",&s1,&s2);
+    
+for(i=0;s1[i]!='\0';i++)
+    
+{
+        
+s2[i]=s1[i];
+    
+    
+}
+    
+s2[i]='\0';
+    
+printf("%s",s2);
+
+}
