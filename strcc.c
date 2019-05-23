4d70452e40272317f05f3af8caf01306e7e2cf07
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-23 20:38:28 +0530

    Add files via upload

---
+++ strcc.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+#include<string.h>
+
+int main()
+{
+   
+char s1[50],s2[50];
+   
+int a,b;
+   
+gets(s1);
+   
+gets(s2);
+   
+a=strlen(s1);
+   
+b=strlen(s2);
+   
+if(a>b)
+  
+    
+printf("%s",s1);
+   
+else
+   
+printf("%s",s2);
+   
+   
+
+}
