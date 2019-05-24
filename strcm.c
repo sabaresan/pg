0857ff7a594ed18556a996d2e10797136f06301f
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-24 09:56:56 +0530

    Add files via upload

---
+++ strcm.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include<string.h>;
+
+int main() {
+
+    
+char a[50],b[50];
+    
+int i=0,j=0;
+    
+gets(a);
+    
+gets(b);
+    
+for(i=0;a[i]!='\0';i++);
+        
+    
+for(j=0;b[j]!='\0';j++,i++)
+    
+{
+        
+a[i]=b[j];
+    
+}
+    
+    
+a[i]='\0';
+    
+printf("%s",a);
+    
+return 0;
+    
+
+}
