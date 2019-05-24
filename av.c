17218ecaa8cc25b4818498e07e290f75667c7063
sabaresan <36839959+sabaresan@users.noreply.github.com>
2019-05-24 11:30:12 +0530

    Add files via upload

---
+++ av.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+
+int main()
+{
+    
+int n,i,sum;
+    
+int avg;
+    
+sum=avg=0;
+    
+scanf("%d",&n);
+    
+int arr[n];
+    
+for(i=0;i<n;i++)
+    
+{
+        
+scanf("%d",&arr[i]);
+        
+sum=sum+arr[i];
+    
+}
+    
+avg=(int)sum/n;
+    
+printf("%d ",avg);
+    
+
+}
